f92639eeff9f3fda0f117293ee0a78bd67eb7241
VedashriH <vedashrih1234@gmail.com>
2023-03-29 12:44:42 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function myFunction() {
+    alert("Data submitted");
+}
+/*
+document.querySelector('.cross').style.display = 'none';
+document.querySelector('.hamburger').addEventListener("click",()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebarGo');
+    if(document.querySelector('sidebar').classList.contains('sidebarGo')){
+        document.querySelector('.ham').style.display='inline';
+        document.querySelector('.cross').style.display='none';
+    }
+    else{
+        document.querySelector('.ham').style.display='none';
+        setTimeout(()=>{
+            document.querySelector('.cross').style.display='inline';
+        }, 300);
+    }
+})*/
+function openNav() {
+    document.getElementById("mySidebar").style.width = "200px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+  }
